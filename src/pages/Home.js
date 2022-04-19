76516afef601ea4144c7e22a04565458d2b94ea9
urahmank <usamak23@gmail.com>
2022-04-20 02:50:48 +0500

    homepage popup add and content change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ import { Card } from "@mui/material";
 import { Link } from "react-router-dom";
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
 // popup dialog box imports
 import PropTypes from 'prop-types';
@@ -21,6 +22,9 @@ import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 // import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
+
+import popupImg from '../assets/img/popupImg.jpg';
+
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -87,8 +91,34 @@ export default function Home() {
     setOpen(false);
   };
 
+  const [openImage, setOpenImage] = React.useState(true);
+
+  const handleClickOpenImage = () => {
+    setOpenImage(true);
+  };
+  const handleCloseImage = () => {
+    setOpenImage(false);
+  };
+
   return (
     <div className="home">
+      {/* image dialog content start */}
+      <BootstrapDialog
+        onClose={handleCloseImage}
+        aria-labelledby="customized-dialog-title"
+        open={openImage}
+      >
+        <DialogContent onClose={handleCloseImage}>
+          <img src={popupImg} alt="popupImg" className="w-100 h-100" />
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleCloseImage} style={{ color: "#70a09f" }}>
+            Close
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+      {/* image dialog content end */}
+
       {/* popup dialog content start */}
       <BootstrapDialog
         onClose={handleClose}
@@ -100,46 +130,100 @@ export default function Home() {
         </BootstrapDialogTitle>
         <DialogContent dividers>
           <Typography gutterBottom>
-            Norsa N.V. ke partisipa ku entrante <b style={{color: "red"}}>01 April 2022</b> sigiente negoshi nan ta asepta <b>UNIKAMENTE</b> Norsa Card pa hasi kompras:
-            <br/>
-            <b>Supermerkado:</b> <b style={{color: "red"}}>(Por hasi kompras UNIKAMENTE na e supermerkado nan aki)</b>
+            Norsa N.V. ke partisipa ku entrante <b style={{ color: "red" }}>01 April 2022</b> sigiente negoshi nan ta asepta <b>UNIKAMENTE</b> Norsa Card pa hasi kompras:
+          </Typography>
+          <br />
+          <Typography gutterBottom>
+            <b>Supermerkado:</b>
           </Typography>
           <Typography gutterBottom>
-            Alves Supermarket <br/>
-            Best Buy Supermarket <br/>
-            Beurs Mini Market <br/>
-            Bon Bini Supermarket <br/>
-            California Supermarket <br/>
-            Cost U Less <br/>
-            Dande <br/>
-            Goisco <br/>
-            Luna Park <br/>
-            Boulevard Supermarket <br/>
+            <Grid container spacing={1}>
+              <Grid item xs={6} md={6}>
+                Alves Supermarket
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Best Buy Supermarket
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Beurs Mini Market
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Bon Bini Supermarket
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Boulevard Supermarket
+              </Grid>
+              <Grid item xs={6} md={6}>
+                California Supermarket
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Cost U Less
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Dande
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Goisco
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Luna Park
+              </Grid>
+            </Grid>
+          </Typography>
+          <br />
+          <Typography gutterBottom>
+            <b>Negoshi nan:</b>
           </Typography>
           <Typography gutterBottom>
-            <b>Negoshi nan: (Tur otro negoshi nan ta tuma bon di papel ahinda)</b>
+            <Grid container spacing={1}>
+              <Grid item xs={6} md={6}>
+                Balanis
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Bon Prijs
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Building Depot
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Complete Home & Electronics
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Coqueta’s
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Kriss Kross
+              </Grid>
+              <Grid item xs={6} md={6}>
+                La Curacao
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Lovely Fashion
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Muebleria di Credito
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Multitronics
+              </Grid>
+              <Grid item xs={6} md={6}>
+                P.K. Electronics
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Real Size
+              </Grid>
+              <Grid item xs={6} md={6}>
+                Senjor Barata
+              </Grid>
+            </Grid>
           </Typography>
+          <br />
           <Typography gutterBottom>
-            Balanis Mommies Angels <br/>
-            Balanis Sta Maria <br/>
-            Bon Prijs <br/>
-            Building Depot <br/>
-            Hello <br/>
-            Kriss Kross <br/>
-            P.K. Electronics <br/>
-            La Curacao Zeelandia <br/>
-            La Curacao Super Store <br/>
-            La Curacao Otrabanda <br/>
-            Muebleria di Credito <br/>
-            Senjor Barata <br/>
-            Super Barata <br/>
-          </Typography>
-          <Typography gutterBottom>
-            <b style={{color: "red"}}>*Nos lo sigi tene boso na altura, kual ta e negoshi nan ku lo sigi bini aserka ku ta uza Norsa Card.</b>
+            <b style={{ color: "red" }}>(Tur otro negoshi nan ta tuma bon di papel ahinda)</b>
           </Typography>
         </DialogContent>
         <DialogActions>
-          <Button autoFocus onClick={handleClose} style={{color: "#70a09f"}}>
+          <Button autoFocus onClick={handleClose} style={{ color: "#70a09f" }}>
             Close
           </Button>
         </DialogActions>
@@ -151,18 +235,18 @@ export default function Home() {
           Komunikado
         </Button>
         <div class="cards">
-          <Link to='/clientForm' style={{all: "unset"}}>
+          <Link to='/clientForm' style={{ all: "unset" }}>
             <div class="card card-1">
-                <div className="buttonText">
-                  Kliente Eksistente
-                </div>
+              <div className="buttonText">
+                Kliente Eksistente
+              </div>
             </div>
           </Link>
-          <Link to='/clientNoboForm' style={{all: "unset"}}>
+          <Link to='/clientNoboForm' style={{ all: "unset" }}>
             <div class="card card-2">
-                <div className="buttonText">
-                  Kliente Nobo
-                </div>
+              <div className="buttonText">
+                Kliente Nobo
+              </div>
             </div>
           </Link>
         </div>
